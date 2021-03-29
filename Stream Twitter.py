700fae00d401b1fc27b43f4aa4224ab0483057fe
eqivalen <58368919+eqivalen@users.noreply.github.com>
2021-03-29 23:51:55 +0700

    Update Stream Twitter.py

--- Stream Twitter.py
+++ Stream Twitter.py
@@ -5,6 +5,8 @@ from tweepy import Stream
 import time
 from datetime import datetime
 import json
+import os
+from art import *
 
 #Daftar akun Developer Twitter dahulu untuk mendapatkan kode. https://developer.twitter.com/ 
 access_token = ""
@@ -16,21 +18,24 @@ class StdoutListener(StreamListener):
     def on_data(self,data):
         try:
             data = json.loads(data)
-            #untuk username yang tweets, masih dalam proses :v
             tweet = data['text']
             now = datetime.now()
             time_string = now.strftime("%H:%M:%S")
             date_string = now.strftime("%d/%m/%Y")
+            tprint('eqivalone',font='bulbhead')
+            print('\n\n\n\n\n\n\n\n\n\n\n\n\n')
+            print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
             print('Tweet :')
-            print('')
             print(tweet)
             print('')
             print("Jam :", time_string)
             print('Tanggal :', date_string)	 
-            print("--------------------------------------------------------------")
+            print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
             #time.sleep(5) #barangkali mau di jeda bisa dihapus tanda (#)
             #nanti bakal ditambahkan pembuatan file csv
 
+            clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+            clearConsole()
             return True
         except BaseException as e:
             print('Gagal',(e))
@@ -43,4 +48,4 @@ l = StdoutListener()
 auth = OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
 stream = Stream(auth,l)
-stream.filter(track=['gabut','indonesia','berita']) #Isi bisa diubah, ya terserah mau berapa banyak.
+stream.filter(track=['kenyataan','berita','kerja','kenapa']) #Isi bisa diubah, ya terserah mau berapa banyak.
